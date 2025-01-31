d378fa76bd3dc013ff0d9b5e02c62d89c994ff6c
Antonio150 <antoniorollande@gmail.com>
2025-01-31 17:30:35 +0300

    eessssss

--- vendor/composer/autoload_real.php
+++ vendor/composer/autoload_real.php
@@ -33,6 +33,18 @@ class ComposerAutoloaderInita88121369df41bb68929601d068184c4
 
         $loader->register(true);
 
+        $filesToLoad = \Composer\Autoload\ComposerStaticInita88121369df41bb68929601d068184c4::$files;
+        $requireFile = \Closure::bind(static function ($fileIdentifier, $file) {
+            if (empty($GLOBALS['__composer_autoload_files'][$fileIdentifier])) {
+                $GLOBALS['__composer_autoload_files'][$fileIdentifier] = true;
+
+                require $file;
+            }
+        }, null, null);
+        foreach ($filesToLoad as $fileIdentifier => $file) {
+            $requireFile($fileIdentifier, $file);
+        }
+
         return $loader;
     }
 }
