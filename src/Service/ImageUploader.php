c9ca39ad5ad2f9a27e7515d46f3ac63f2cd4744b
Antonio150 <antoniorollande@gmail.com>
2025-02-01 08:21:46 +0300

    aaza

--- src/Service/ImageUploader.php
+++ src/Service/ImageUploader.php
@@ -7,16 +7,23 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 class ImageUploader
 {
     private string $uploadDir;
+    private string $publicPath;
 
-    public function __construct(string $uploadDir)
+    public function __construct(string $uploadDir, string $publicPath)
     {
         $this->uploadDir = $uploadDir;
+        $this->publicPath = $publicPath;
     }
 
-    public function upload(UploadedFile $file): string
+    public function upload(UploadedFile $file): array
     {
-        $fileName = uniqid().'.'.$file->guessExtension();
+        $fileName = uniqid() . '.' . $file->guessExtension();
         $file->move($this->uploadDir, $fileName);
-        return $fileName;
+
+        return [
+            'fileName' => $fileName,
+            'absolutePath' => $this->uploadDir . '/' . $fileName,
+            'publicPath' => $this->publicPath . '/' . $fileName
+        ];
     }
 }
