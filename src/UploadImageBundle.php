1bbf8731424046acf8d2486b34484924c2e707ca
Antonio150 <antoniorollande@gmail.com>
2025-02-03 19:16:24 +0300

    edit bundle e

--- src/UploadImageBundle.php
+++ src/UploadImageBundle.php
@@ -7,9 +7,12 @@ use Symfony\Component\HttpKernel\Bundle\Bundle;
 
 class UploadImageBundle extends Bundle
 {
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+    }
     protected function getContainerExtensionClass(): string
     {
         return 'UploadImageBundle\DependencyInjection\UploadImageExtension'; // ✅ Vérifie bien le chemin
     }
-
 }
