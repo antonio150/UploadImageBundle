d74042d6a2d333d810b82a2969144fb241ea57a7
Antoniorollande <antonio@devsoft-pro.com>
2025-04-22 21:02:32 +0300

    amelioration

--- src/Service/FileUploader.php
+++ src/Service/FileUploader.php
@@ -11,6 +11,7 @@ class FileUploader
     private string $documentsDir;
     private string $videosDir;
     private string $imagesDir;
+    private ParameterBagInterface $params;
     private Filesystem $filesystem;
 
     public function __construct(ParameterBagInterface $params, Filesystem $filesystem)
@@ -19,9 +20,10 @@ class FileUploader
         $this->videosDir = $params->get('upload_image_bundle.videos_dir');
         $this->imagesDir = $params->get('upload_image_bundle.images_dir');
         $this->filesystem = $filesystem;
+        $this->params = $params;
     }
 
-    public function upload(UploadedFile $file): array
+    public function upload( UploadedFile $file): array
     {
         // Détection automatique du type en fonction du MIME type
         $mimeType = $file->getMimeType();
@@ -33,10 +35,16 @@ class FileUploader
         // Déplacer le fichier vers le bon répertoire
         $file->move($targetDir, $fileName);
 
+        // Calculer le chemin relatif pour public_path
+        $projectDir = realpath($this->params->get('kernel.project_dir') . '/public');
+        $absoluteTargetDir = realpath($targetDir);
+        $relativePath = str_replace($projectDir, '', $absoluteTargetDir) . '/' . $fileName;
+        $publicPath = str_replace('\\', '/', $relativePath);
+
         return [
             'fileName' => $fileName,
             'absolute_path' => realpath($targetDir . '/' . $fileName),
-            'public_path' => '/uploads/' . $fileName
+            'public_path' => $publicPath
         ];
     }
 
